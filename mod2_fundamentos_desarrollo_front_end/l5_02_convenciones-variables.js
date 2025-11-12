76518a6e88b6e8f1bceca243e4c2a4de9c2e564c
Daniela Nardecchia <misskumi123@gmail.com>
2025-11-12 20:54:06 -0300

    Add files via upload

---
+++ mod2_fundamentos_desarrollo_front_end/l5_02_convenciones-variables.js
@@ -0,0 +1,23 @@
+// UpperCamelCase: una forma de escribir nombres de variables 
+let NombreYApellidoDelAlumno;
+
+//camelCase: otra forma, solo q la 1era letra es minuscula 
+//let es la palabra para definir que creo una variable 
+//esta se usa en JavaScript
+let nombreYApellidoDelAlumno;
+
+//snakeCase: otra forma de escribir variables  
+let nombre-y-apellido-del-alumno;
+
+
+
+let valor=90; //numero
+let nombre="Daniela" //string
+let siCumple=true //booleano
+
+//Con const declaro constantes!! 
+const iva = 19;
+
+//para cambiar una variable solo debo volver a asignarla, sin usar let 
+valor= 100 //con eso ya cambio 
+
